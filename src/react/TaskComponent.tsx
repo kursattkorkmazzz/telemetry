ac3a384811d8755189aa78a497842799589f6887
Kürşat KORKMAz <kursat.korkmaz123@hotmail.com>
2025-03-10 15:13:52 +0300

    ComponentEventCollector ve TaskComponent yapıldı ama başka birşey deneneceği için save alındı.

--- src/react/TaskComponent.tsx
+++ src/react/TaskComponent.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from "react";
-import { PrintConsolePublisher, Task } from "src/core";
+import React from "react";
 import { createContext, useState, useContext } from "react";
 import { AbstractPublisher } from "src/core/core/abstracts/AbstractPublisher";
-import { AbstractScheduler } from "src/core/core/abstracts/AbstractScheduler";
-import { AbstractMetricCollector } from "src/core/core/abstracts/AbstractMetricCollector";
 
 type TaskContextType = {
-  setPublisher: (publisher: AbstractPublisher) => void;
-  setScheduler: (scheduler: AbstractScheduler) => void;
-  setMetricCollectors: (metrics: AbstractMetricCollector[]) => void;
+  publish: (data: unknown) => void;
+  addPublisher: (publisher: AbstractPublisher) => void;
 };
 
 export const TaskContext = createContext<TaskContextType | null>(null);
 
 export function useTaskContext() {
   const taskContext = useContext(TaskContext);
-
   if (!taskContext) {
     throw new Error("useTaskContext must be used within a TaskComponent");
   }
@@ -23,16 +18,21 @@ export function useTaskContext() {
 }
 
 export function TaskComponent(props: { children?: React.ReactNode }) {
-  const [publisher, setPublisher] = useState<AbstractPublisher | null>(null);
-  const [scheduler, setScheduler] = useState<AbstractScheduler | null>(null);
-  const [metrics, setMetrics] = useState<AbstractMetricCollector[]>([]);
+  const [publishers, setPublishers] = useState<AbstractPublisher[]>([]);
 
   return (
     <TaskContext.Provider
       value={{
-        setPublisher,
-        setScheduler,
-        setMetricCollectors: setMetrics,
+        addPublisher(publisher) {
+          setPublishers([...publishers, publisher]);
+        },
+        publish(data: unknown) {
+          console.log("DATA: " + JSON.stringify(data, null, 3));
+
+          publishers.forEach((publisher) => {
+            publisher.publish(data);
+          });
+        },
       }}
     >
       {props.children}
--- src/react/collectors/ComponentEventCollector.tsx
+++ src/react/collectors/ComponentEventCollector.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { HTMLAttributes } from "react";
 import { CounterMetricCollector } from "src/core";
 import { useTelemetryConfiguration } from "../configurer/TelemetryConfigurationManager";
+import { MetricDataStorage } from "src/core/core/libs/storage/MetricDataStorage";
+import { useTaskContext } from "../TaskComponent";
 
 type ComponentEventCollectorProps = HTMLAttributes<HTMLDivElement> & {
   tracking_event: (keyof HTMLElementEventMap)[];
@@ -10,11 +12,18 @@ type ComponentEventCollectorProps = HTMLAttributes<HTMLDivElement> & {
 
 export function ComponentEventCollector(props: ComponentEventCollectorProps) {
   const configuration = useTelemetryConfiguration();
+  const taskContext = useTaskContext();
+
+  const [publishHandler, setPublisherHandler] = useState<() => void>(() => {});
+  const [sending, setSending] = useState(false);
   const parentRef = useRef<HTMLDivElement>(null);
 
   const [metricCollectorList, setMetricCollectors] = useState<
     CounterMetricCollector[]
   >([]);
+  const [metricStorage, setMetricStorage] = useState<MetricDataStorage>(
+    new MetricDataStorage()
+  );
 
   useEffect(() => {
     if (parentRef.current) {
@@ -47,27 +56,46 @@ export function ComponentEventCollector(props: ComponentEventCollectorProps) {
           });
         });
       });
-
-      // 1. Adjusting publishing data scheduling.
     }
   }, [parentRef]);
 
   // 2. Adjusting publishing data scheduling.
   useEffect(() => {
-    console.log("Mount Effect");
-
-    window.addEventListener("beforeunload", () => {
-      //console.log("AAAAA");
-    });
-
+    window.addEventListener("beforeunload", publishHandler);
+    window.addEventListener("close", publishHandler);
     return () => {
-      console.log("Unmount Effect");
-      // Restore events.
+      window.removeEventListener("beforeunload", publishHandler);
+      window.addEventListener("close", publishHandler);
     };
-  }, []);
+  }, [metricCollectorList]);
 
   useEffect(() => {
-    console.log(JSON.stringify(metricCollectorList, null, 3));
+    setPublisherHandler(() => {
+      setSending(true);
+      const mds = new MetricDataStorage();
+      metricCollectorList.forEach((metric) => {
+        mds.add({
+          data: metric.counter,
+          metric_name: metric.metric_name,
+          labels: metric.labels,
+        });
+      });
+      taskContext.publish(mds.getAll());
+    });
   }, [metricCollectorList]);
+
+  useEffect(() => {
+    if (sending) {
+      setMetricCollectors((prev) => {
+        const filtered = prev.map((mc) => {
+          mc.counter = 0;
+          return mc;
+        });
+
+        return [...filtered];
+      });
+      setSending(false);
+    }
+  }, [sending]);
   return <div {...props} ref={parentRef} />;
 }
